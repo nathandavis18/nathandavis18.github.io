749453f2b7fdbf3b0da3c295c32d06050f110527
Nathan Davis <144376756+nathandavis18@users.noreply.github.com>
2025-01-31 14:39:21 -0700

    Now its mobile-friendly

--- src/app/navigation.js
+++ src/app/navigation.js
@@ -1,7 +1,7 @@
 'use client';
 
-import {Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/react';
-import {ChevronDownIcon} from '@heroicons/react/20/solid';
+import {Menu, MenuButton, MenuItem, MenuItems, Disclosure, DisclosureButton, DisclosurePanel, CloseButton} from '@headlessui/react';
+import {ChevronDownIcon, Bars3Icon} from '@heroicons/react/24/solid';
 import Link from 'next/link'
 import {usePathname} from 'next/navigation';
 
@@ -22,16 +22,25 @@ function getLinkStyle(linkPath){
     }
     return otherPage;
 }
+
 export default function Navigation(){
     const gamesNavButton = `${getLinkStyle("/Games")} inline-flex items-center rounded-md`;
     const projectsNavButton = `${getLinkStyle("/Projects")} inline-flex items-center rounded-md`;
     return(
-        <>
-        <nav className="bg-white bg-zinc-900 py-2">
-            <div className="max-w-screen-xl flex flex-wrap items-center mx-auto p-4 text-xl">
-                <Link href="/" className="self-center text-3xl bg-gradient-to-r from-red-500 via-red-300 to-white text-transparent bg-clip-text" style={{textDecoration: 'none'}}>Nathan Davis</Link>
-                <div className="pl-8 hidden md:block md:w-auto" id="navbar-default">
-                    <ul className="font-medium flex flex-col md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0" style={{listStyleType: 'none'}}>
+    <>
+        <Disclosure as="nav" className="bg-white bg-zinc-900 py-2">
+            <div className="flex flex-wrap p-4 pt-3 md:pt-4 text-xl mx-auto md:max-w-screen-xl">
+                <Link href="/" className="md:flex-initial self-center text-3xl bg-gradient-to-r from-red-500 via-red-300 to-white text-transparent bg-clip-text w-3/5 md:w-auto pt-1 md:pt-0" style={{textDecoration: 'none'}}>Nathan Davis</Link>
+                <div className="w-2/5 md:hidden place-items-end md:w-auto">
+                    <div>
+                        <DisclosureButton className="md:hidden pt-2 group rounded-md text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
+                            <span className="sr-only">Open main menu</span>
+                            <Bars3Icon aria-hidden="true" className="block size-6" />
+                        </DisclosureButton>
+                    </div>
+                </div>
+                <div className="pl-8 hidden md:block md:pt-1">
+                    <ul className="font-medium flex md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0" style={{listStyleType: 'none'}}>
                         <li className="max-w-medium">
                             <Link href="/" title="About Me" className={getLinkStyle("/")} style={{textDecoration: 'none'}}>Home</Link>
                         </li>
@@ -73,8 +82,52 @@ export default function Navigation(){
                     </ul>
                 </div>
             </div>
-        </nav>
+            <DisclosurePanel className="md:hidden">
+                <div className="space-y-1 px-2 pt-2 pb-3">
+                    <ul>
+                        <li className="max-w-medium">
+                            <CloseButton as={Link} href="/" title="About Me" className={getLinkStyle("/")} style={{textDecoration: 'none'}}>Home</CloseButton>
+                        </li>
+                        <li className="max-w-medium">
+                            <CloseButton as={Link} href="/Resume" title="My Resume/CV" className={getLinkStyle("/Resume")} style={{textDecoration: 'none'}}>Resume/CV</CloseButton>
+                        </li>
+                        <li className="max-w-medium">
+                            <Menu>
+                                <MenuButton className={projectsNavButton}>
+                                    Projects
+                                    <ChevronDownIcon className="size-5" />
+                                </MenuButton>
+                                <MenuItems anchor="bottom" className="px-6 origin-top-middle rounded-xl bg-zinc-800 py-5">
+                                    <MenuItem className="mb-2">
+                                        <CloseButton as={Link} href="/Projects/Project-List" className={getLinkStyle("/Projects/Project-List")} style={{textDecoration: 'none'}}>Project List</CloseButton>
+                                    </MenuItem>
+                                    <MenuItem className="mb-2">
+                                        <CloseButton as={Link} href="/Projects/NotVim-Editor" className={getLinkStyle("/Projects/NotVim-Editor")} style={{textDecoration: 'none'}}>NotVim-Editor</CloseButton>
+                                    </MenuItem>
+                                </MenuItems>
+                            </Menu>
+                        </li>
+                        <li className="max-w-medium">
+                            <Menu>
+                                <MenuButton className={gamesNavButton}>
+                                    Games
+                                    <ChevronDownIcon className="size-5" />
+                                </MenuButton>
+                                <MenuItems anchor="bottom" className="px-6 origin-top-middle rounded-xl bg-zinc-800 py-5">
+                                    <MenuItem className="mb-2">
+                                        <CloseButton as={Link} href="/Games/Snake" className={getLinkStyle("/Games/Snake")} style={{textDecoration: 'none'}}>Snake</CloseButton>
+                                    </MenuItem>
+                                </MenuItems>
+                            </Menu>
+                        </li>
+                        <li className="max-w-medium">
+                            <CloseButton as="a" href="mailto:nathandavis@nathandavis18.com" title="Email Me" className={otherPage} style={{textDecoration: 'none'}}>Email</CloseButton>
+                        </li>
+                    </ul>
+                </div>
+            </DisclosurePanel>
+        </Disclosure>
         <hr />
-        </>
+    </>
     );
 }
